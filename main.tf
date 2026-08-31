3864fcfb648553abd5f5074b335112f76354d297
A S M Saifool Islam <asmsaifool.islam@nttdata.com>
2026-08-31 14:02:53 -0400

    feat!: convert to copier-managed skeleton (#16)
    * feat!: convert to copier-managed skeleton
    
    * fix: remove duplicate automatic_channel_upgrade and load_balancer_sku attributes
    
    ---------
    
    Signed-off-by: A S M Saifool Islam <asmsaifool.islam@nttdata.com>

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ module "aks" {
   identity_type                   = var.identity_type
   identity_ids                    = var.identity_ids
   kubernetes_version              = var.kubernetes_version
+  automatic_channel_upgrade       = var.automatic_channel_upgrade
   cluster_name                    = var.cluster_name
   api_server_subnet_id            = var.api_server_subnet_id
   api_server_authorized_ip_ranges = var.api_server_authorized_ip_ranges
@@ -114,7 +115,6 @@ module "aks" {
   admin_username                                                  = var.admin_username
   agents_pool_kubelet_configs                                     = var.agents_pool_kubelet_configs
   agents_proximity_placement_group_id                             = var.agents_proximity_placement_group_id
-  automatic_channel_upgrade                                       = var.automatic_channel_upgrade
   azure_policy_enabled                                            = var.azure_policy_enabled
   cluster_log_analytics_workspace_name                            = var.cluster_log_analytics_workspace_name
   confidential_computing                                          = var.confidential_computing
@@ -126,8 +126,8 @@ module "aks" {
   kms_enabled                                                     = var.kms_enabled
   kms_key_vault_key_id                                            = var.kms_key_vault_key_id
   kms_key_vault_network_access                                    = var.kms_key_vault_network_access
-  load_balancer_profile_enabled                                   = var.load_balancer_profile_enabled
   load_balancer_sku                                               = var.load_balancer_sku
+  load_balancer_profile_enabled                                   = var.load_balancer_profile_enabled
   load_balancer_profile_idle_timeout_in_minutes                   = var.load_balancer_profile_idle_timeout_in_minutes
   load_balancer_profile_managed_outbound_ip_count                 = var.load_balancer_profile_managed_outbound_ip_count
   load_balancer_profile_managed_outbound_ipv6_count               = var.load_balancer_profile_managed_outbound_ipv6_count
